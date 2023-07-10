530e6447e022aca2f51b108e6881aa97ab93e35a
Nazarii Korchevskyi <nazar10fantom@gmail.com>
2023-07-10 21:56:05 +0200

    bound the menu to the scroll

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,9 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Link from '../Link/Link';
 import style from './Menu.module.scss';
 
 function Menu({ skills, projects }) {
 	const [activeLink, setActiveLink] = useState('about');
+
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY >= skills.current.offsetTop && window.scrollY < projects.current.offsetTop) {
+				setActiveLink(skills);
+			} else if (window.scrollY >= projects.current.offsetTop) {
+				setActiveLink(projects);
+			} else {
+				setActiveLink('about');
+			}
+		};
+		window.addEventListener('scroll', handleScroll);
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
 
 	return (
 		<nav className={style.nav}>
